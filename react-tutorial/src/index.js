88751419bc9c9aaffad85a0a04821002d0d86544
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-25 08:18:45 +0100

    changing sihfts

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -15,13 +15,15 @@ class Board extends Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   };
   
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares })
+    squares[i] = this.state.xIsNext ? 'X': 'O';
+    this.setState({ squares: squares });
+    this.setState({ xIsNext: !this.state.xIsNext });
   };
 
   renderSquare(i) {
@@ -34,7 +36,7 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
 
     return (
       <div>

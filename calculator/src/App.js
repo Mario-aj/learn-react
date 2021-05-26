6d051cb38f6e0d173ebbf98390bf305348204db6
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-26 11:51:22 +0100

    config font

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,7 +3,7 @@ import CalculatorButtons from './components/CalculatorButtons';
 import Screen from './components/Screnn';
 
 const App = () => (
-  <div className="flex items-center justify-center w-screen h-screen py-4 bg-gray-300">
+  <div className="flex items-center justify-center w-screen h-screen py-4 font-sans bg-gray-300">
     <div className="relative flex flex-col items-center justify-center h-full px-4 py-16 bg-white rounded-2xl w-80">
       <div className="absolute flex flex-col items-center justify-center top-2">
         <div className="w-2 h-2 mb-2 bg-black rounded-full" />
--- calculator/tailwind.config.js
+++ calculator/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
   },
   variants: {
     extend: {

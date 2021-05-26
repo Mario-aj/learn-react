3d80008282c97700bd90162bc72867cfa5ec0ec6
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-26 12:22:51 +0100

    small refactor

--- calculator/src/components/CalculatorButtons.js
+++ calculator/src/components/CalculatorButtons.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Button from './Button';
-import WrapperButton from './WrapperButton';
+import Button from './Button/Button';
+import WrapperButton from './Button/WrapperButton';
 
 const CalculatorButtons = () => (
   <div className="flex flex-col w-full">

38b930b99e6f55822f5228eb4745d80761895f59
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-26 09:04:31 +0100

    active eslint and prettier in the project and format all files

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,7 +1,5 @@
-const App = () => {
-  return (
-    <div className="m-auto text-5xl text-gray-600">Mario</div>
-  );
-}
+import React from 'react';
+
+const App = () => <div className="m-auto text-5xl text-gray-600">Mario</div>;
 
 export default App;

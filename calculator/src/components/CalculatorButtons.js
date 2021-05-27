682ad17359a818d36ba509224beea9a7c0ef5392
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-05-27 06:40:17 +0100

    create new functions

--- calculator/src/components/CalculatorButtons.js
+++ calculator/src/components/CalculatorButtons.js
@@ -3,122 +3,89 @@ import React from 'react';
 import Button from './Button/Button';
 import WrapperButton from './Button/WrapperButton';
 
-const SYMBOLS = ['/', '*', '+', '-', '=', '%'];
-
 const CalculatorButtons = () => {
-  let acumulator = Array(2);
+  let acumulator = Array(2).fill(0);
 
-  const handleClick = (value) => {
-    if (value === 'AC') acumulator = [];
-
-    if (SYMBOLS.includes(value)) {
-      acumulator[1] = value;
-      console.log(acumulator);
-    } else {
-      const prev = String();
-      console.log(prev);
-      acumulator[0] = Number(prev + '' + value);
-    }
-  };
+  const handleNumber = (value) => {};
+  const handleClean = () => {};
+  const handlePercentage = () => {};
+  const handleDiv = () => {};
+  const handleEqual = () => {};
+  const handleSum = () => {};
+  const handleSub = () => {};
+  const handleMult = () => {};
+  const handleDot = () => {};
 
   return (
     <div className="flex flex-col w-full">
       <WrapperButton>
-        <Button
-          text="AC"
-          intent="secondary"
-          onClick={() => handleClick('AC')}
-        />
-        <Button
-          text="+/-"
-          intent="secondary"
-          onClick={() => handleClick('+/-')}
-        />
-        <Button text="%" intent="secondary" onClick={() => handleClick('%')} />
-        <Button
-          text="/"
-          intent="symbol"
-          textColor
-          onClick={() => handleClick('/')}
-        />
+        <Button text="AC" intent="secondary" onClick={handleClean} />
+        <Button text="+/-" intent="secondary" onClick={() => {}} />
+        <Button text="%" intent="secondary" onClick={handlePercentage} />
+        <Button text="/" intent="symbol" textColor onClick={handleDiv} />
       </WrapperButton>
       <WrapperButton>
         <Button
           text="7"
           intent="primary"
           textColor
-          onClick={() => handleClick(7)}
+          onClick={() => handleNumber(7)}
         />
         <Button
           text="8"
           intent="primary"
           textColor
-          onClick={() => handleClick(8)}
+          onClick={() => handleNumber(8)}
         />
         <Button
           text="9"
           intent="primary"
           textColor
-          onClick={() => handleClick(9)}
+          onClick={() => handleNumber(9)}
         />
-        <Button
-          text="x"
-          intent="symbol"
-          textColor
-          onClick={() => handleClick('*')}
-        />
+        <Button text="x" intent="symbol" textColor onClick={handleMult} />
       </WrapperButton>
       <WrapperButton>
         <Button
           text="4"
           intent="primary"
           textColor
-          onClick={() => handleClick(4)}
+          onClick={() => handleNumber(4)}
         />
         <Button
           text="5"
           intent="primary"
           textColor
-          onClick={() => handleClick(5)}
+          onClick={() => handleNumber(5)}
         />
         <Button
           text="6"
           intent="primary"
           textColor
-          onClick={() => handleClick(6)}
+          onClick={() => handleNumber(6)}
         />
-        <Button
-          text="-"
-          intent="symbol"
-          textColor
-          onClick={() => handleClick('-')}
-        />
+        <Button text="-" intent="symbol" textColor onClick={handleSub} />
       </WrapperButton>
       <WrapperButton>
         <Button
           text="1"
           intent="primary"
           textColor
-          onClick={() => handleClick(1)}
+          onClick={() => handleNumber(1)}
         />
         <Button
           text="2"
           intent="primary"
           textColor
-          onClick={() => handleClick(2)}
+          onClick={() => handleNumber(2)}
         />
         <Button
           text="3"
           intent="primary"
           textColor
-          onClick={() => handleClick(3)}
+          onClick={() => handleNumber(3)}
         />
-        <Button
-          text="+"
-          intent="symbol"
-          textColor
-          onClick={() => handleClick('+')}
-        />
+        <Button text="+" intent="symbol" textColor onClick={handleSum} />
       </WrapperButton>
       <WrapperButton>
         <Button
@@ -126,20 +93,10 @@ const CalculatorButtons = () => {
           intent="primary"
           textColor
           className="w-32 pl-6 text-left"
-          onClick={() => handleClick(0)}
+          onClick={() => handleNumber(0)}
         />
-        <Button
-          text=","
-          intent="primary"
-          textColor
-          onClick={() => handleClick('.')}
-        />
-        <Button
-          text="="
-          intent="symbol"
-          textColor
-          onClick={() => handleClick('=')}
-        />
+        <Button text="," intent="primary" textColor onClick={handleDot} />
+        <Button text="=" intent="symbol" textColor onClick={handleEqual} />
       </WrapperButton>
     </div>
   );

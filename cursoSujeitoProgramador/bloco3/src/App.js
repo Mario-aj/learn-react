209c68bddf0375a22e8fabeaafb8dc238e2425d8
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-02 09:34:49 +0100

    work with lists in reactjs

--- cursoSujeitoProgramador/bloco3/src/App.js
+++ cursoSujeitoProgramador/bloco3/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 
 import Member from "./components/Member";
 import ConditionalRender from "./components/ConditionalRender";
+import Lists from "./components/Lists";
+
 const App = () => {
   return (
     <>
       <Member name="Visitante" />
       <ConditionalRender />
+      <Lists />
     </>
   );
 };

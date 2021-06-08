3f225a2746c83a4902a8323a0d4cbd1e0af08f4e
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-08 06:34:50 +0100

    form with validation

---
+++ cursoSujeitoProgramador/bloco3/src/components/form/BasicValidation.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class BasicValidation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const { name } = this.state;
+    alert({ name });
+  }
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <div>
+        <h1>New Form with validation</h1>
+
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Name: </label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default BasicValidation;

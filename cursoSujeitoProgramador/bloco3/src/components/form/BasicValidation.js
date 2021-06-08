dcd17584bdd8ea6ea774768168d9ad519461613c
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-08 06:54:52 +0100

    apply basic validation

--- cursoSujeitoProgramador/bloco3/src/components/form/BasicValidation.js
+++ cursoSujeitoProgramador/bloco3/src/components/form/BasicValidation.js
@@ -5,6 +5,9 @@ class BasicValidation extends Component {
     super(props);
     this.state = {
       name: "",
+      email: "",
+      password: "",
+      error: "",
     };
 
     this.onSubmit = this.onSubmit.bind(this);
@@ -12,17 +15,21 @@ class BasicValidation extends Component {
 
   onSubmit(e) {
     e.preventDefault();
-    const { name } = this.state;
-    alert(name);
+    const { name, email, password } = this.state;
+
+    if (name.trim() && email.trim() && password.trim()) {
+      this.setState({ error: "" });
+      alert(`name: ${name} \nemail: ${email} \n password: ${password}`);
+    } else this.setState({ error: "Ops! something is missing!" });
   }
 
   render() {
-    const { name } = this.state;
+    const { name, email, password, error } = this.state;
 
     return (
       <div>
         <h1>New Form with validation</h1>
-
+        {error && <p>{error}</p>}
         <form onSubmit={this.onSubmit}>
           <div>
             <label>Name: </label>
@@ -30,6 +37,24 @@ class BasicValidation extends Component {
               type="text"
               value={name}
               onChange={(e) => this.setState({ name: e.target.value })}
+            />
+          </div>
+
+          <div>
+            <label>E-mail: </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+          </div>
+
+          <div>
+            <label>Password: </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => this.setState({ password: e.target.value })}
             />
           </div>
           <br />

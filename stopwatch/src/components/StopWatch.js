95ddb10a58e3bfe6110ba093780d89b7f9b69b4b
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-09 07:41:00 +0100

    fineshed the logic for play stopwatch

--- stopwatch/src/components/StopWatch.js
+++ stopwatch/src/components/StopWatch.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Component } from "react";
-import { FiCircle, FiPlay, FiPower } from "react-icons/fi";
+import { FiCircle, FiPlay, FiPause, FiPower } from "react-icons/fi";
 
 import "./index.css";
 
@@ -9,17 +9,42 @@ class StopWatch extends Component {
     super(props);
     this.state = {
       watch: 0,
+      isRunning: false,
     };
+    this.timer = null;
+    this.Play = this.Play.bind(this);
+  }
+
+  Play() {
+    if (this.timer === null) {
+      this.timer = setInterval(() => {
+        let { watch } = this.state;
+        watch += 0.1;
+        this.setState({ watch });
+      }, 100);
+
+      this.setState({ isRunning: true });
+    } else {
+      clearInterval(this.timer);
+      this.timer = null;
+      this.setState({ isRunning: false });
+    }
   }
 
   render() {
+    const { watch, isRunning } = this.state;
+
     return (
       <div className="container">
         <FiCircle color="#fff" size={320} className="circle" />
-        <p className="stopwatch">00:00</p>
+        <p className="stopwatch">{watch.toFixed(1)}</p>
         <div className="btn-container">
-          <a className="button">
-            <FiPlay color="#fff" size={50} />
+          <a onClick={this.Play} className="button">
+            {isRunning ? (
+              <FiPause color="#fff" size={50} />
+            ) : (
+              <FiPlay color="#fff" size={50} />
+            )}
           </a>
 
           <a className="button">

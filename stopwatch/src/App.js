25e9c8a6526a4ea68c30d0c9175171879585ff61
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-09 07:10:02 +0100

    mount the structure of program

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,5 +1,24 @@
+import "./index.css";
+import { FiCircle, FiPlay, FiPause, FiPower } from "react-icons/fi";
+
 function App() {
-  return <div className="App">Stopwatch</div>;
+  return (
+    <div className="App">
+      <div>
+        <FiCircle color="#fff" size={320} />
+        <p className="stopWatch">00:00</p>
+        <div>
+          <a className="button">
+            <FiPlay color="#fff" size={50} />
+          </a>
+
+          <a className="button">
+            <FiPower color="#fff" size={50} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

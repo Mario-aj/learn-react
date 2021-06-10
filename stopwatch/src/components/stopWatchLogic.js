efec15b484d03d589f13d9c201228e8229e8adcd
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-10 17:10:36 +0100

    pt constructor into main function

--- stopwatch/src/components/stopWatchLogic.js
+++ stopwatch/src/components/stopWatchLogic.js
@@ -1,6 +1,11 @@
 import EndCounting from "./EndCounting";
 
 class StopWhatLogic extends EndCounting {
+  constructor(props) {
+    super(props);
+    this.formatedNumber = this.formatedNumber.bind(this);
+  }
+
   formatedNumber(number) {
     return number >= 10 ? number : `0${number}`;
   }

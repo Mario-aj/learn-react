9c8d2b10acf3edb8deaa191349fc962c6593fd45
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-10 17:02:52 +0100

    separate the logic that start counting

---
+++ stopwatch/src/components/Start.js
@@ -0,0 +1,39 @@
+import { Component } from "react";
+
+class Start extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      watch: 0,
+      isRunning: false,
+      minute: 0,
+    };
+
+    this.timer = null;
+    this.StartAndStop = this.StartAndStop.bind(this);
+  }
+
+  StartAndStop() {
+    if (this.timer === null) {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        if (state.watch.toFixed(1) === "59.9") {
+          state.minute += 1;
+          state.watch = 0;
+          this.setState(this.state);
+        } else {
+          state.watch += 0.1;
+          this.setState(state);
+        }
+      }, 100);
+
+      this.setState({ isRunning: true });
+    } else {
+      clearInterval(this.timer);
+      this.timer = null;
+      this.setState({ isRunning: false });
+    }
+  }
+}
+
+export default Start;
--- stopwatch/src/components/stopWatchLogic.js
+++ stopwatch/src/components/stopWatchLogic.js
@@ -1,42 +1,6 @@
-import { Component } from "react";
+import Start from "./Start";
 
-class Play extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      watch: 0,
-      isRunning: false,
-      minute: 0,
-    };
-
-    this.timer = null;
-    this.StartAndStop = this.StartAndStop.bind(this);
-  }
-
-  StartAndStop() {
-    if (this.timer === null) {
-      this.timer = setInterval(() => {
-        let state = this.state;
-        if (state.watch.toFixed(1) === "59.9") {
-          state.minute += 1;
-          state.watch = 0;
-          this.setState(this.state);
-        } else {
-          state.watch += 0.1;
-          this.setState(state);
-        }
-      }, 100);
-
-      this.setState({ isRunning: true });
-    } else {
-      clearInterval(this.timer);
-      this.timer = null;
-      this.setState({ isRunning: false });
-    }
-  }
-}
-
-class PowerOff extends Play {
+class PowerOff extends Start {
   constructor(props) {
     super(props);
     this.Clear = this.Clear.bind(this);

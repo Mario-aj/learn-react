0a46203c9c8c88c290d30fd7954c6720713079d0
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-18 10:04:41 +0100

    cautch multiple params with useParams hook

--- react-router/src/routes.js
+++ react-router/src/routes.js
@@ -5,7 +5,6 @@ import Home from "./pages/Home";
 import User from "./pages/User";
 import About from "./pages/About";
 import Topics from "./pages/Topics";
-import Teste from "./pages/Teste";
 
 const Routes = () => {
   return (
@@ -16,7 +15,6 @@ const Routes = () => {
       <Route path="/about" exact component={About} />
       <Route path="/user" component={User} />
       <Route path="/topics" component={Topics} />
-      <Route path="/test/:name/:age/:nationality" component={Teste} />
     </Switch>
   );
 };

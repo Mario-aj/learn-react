a192d35387899458a4ee7931716e5fbef274af25
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-18 19:12:37 +0100

    authenticated routes

--- react-router/src/routes.js
+++ react-router/src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Home from "./pages/Home";
 import User from "./pages/User";
@@ -7,12 +7,25 @@ import About from "./pages/About";
 import Topics from "./pages/Topics";
 import Teste from "./pages/Teste";
 
+import { authenticated } from "./utils/atuh";
+
+const PriveteRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return authenticated() ? <Component {...props} /> : <Redirect to="/" />;
+      }}
+    />
+  );
+};
+
 const Routes = () => {
   return (
     <Switch>
-      <Route path="/about" component={About} />
-      <Route path="/user" component={User} />
-      <Route path="/topics" component={Topics} />
+      <PriveteRoute path="/about" component={About} />
+      <PriveteRoute path="/user" component={User} />
+      <PriveteRoute path="/topics" component={Topics} />
       <Route path="/test/:name/:age/:nationality" component={Teste} />
       <Route path="/" exact>
         <Home />
---
+++ react-router/src/utils/atuh.js
@@ -0,0 +1,3 @@
+export const authenticated = () => {
+  return false;
+};

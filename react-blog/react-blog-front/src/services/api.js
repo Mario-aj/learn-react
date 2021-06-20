fa0cdd851dd467f1beb89094d0e5f5600dbef1c8
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-06-20 20:57:06 +0100

    create private routes and connect front with our server

---
+++ react-blog/react-blog-front/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:4321',
+});
+
+class API {
+  authenticated() {
+    const storagedData = localStorage.getItem('react-blog');
+
+    return storagedData.token ? true : false;
+  }
+}
+
+export default new API();

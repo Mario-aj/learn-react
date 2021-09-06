b174b3cc6872af486258b5901ee56754fc606cb0
Mario-aj <mariojorge1997@mail.ru>
2021-09-06 10:08:35 +0100

    netflix-clone: styles for movie row list

--- netflix-clone/src/components/MovieRow/MovieRow.js
+++ netflix-clone/src/components/MovieRow/MovieRow.js
@@ -1,16 +1,24 @@
+import * as S from './styled';
+
 const MovieRow = ({ movieLists, title }) => {
   return (
-    <div>
+    <S.MovieRow>
       <h2>{title}</h2>
-      {movieLists.results.length > 0 &&
-        movieLists.results.map((movieList, index) => (
-          <img
-            key={index}
-            src={`https://image.tmdb.org/t/p/w300${movieList.poster_path}`}
-            alt={movieList.original_title}
-          />
-        ))}
-    </div>
+      <S.ListArea>
+        <S.List>
+          {movieLists.results.length > 0 &&
+            movieLists.results.map((movieList, index) => (
+              <div>
+                <img
+                  key={index}
+                  src={`https://image.tmdb.org/t/p/w300${movieList.poster_path}`}
+                  alt={movieList.original_title}
+                />
+              </div>
+            ))}
+        </S.List>
+      </S.ListArea>
+    </S.MovieRow>
   );
 };
 

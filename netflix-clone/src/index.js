f840ee418dfc731aecee827a74241a0339c2563f
Mario-aj <mariojorge1997@mail.ru>
2021-09-06 09:42:07 +0100

    netflix-clone: add styled-component and create global styles

--- netflix-clone/src/index.js
+++ netflix-clone/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { GlobalStyles } from './styles';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root')
 );

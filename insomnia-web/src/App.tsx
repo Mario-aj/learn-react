49dd5055c1a182fdeae89282f760ca39bc0411f0
Mario-aj <mariojorge1997@mail.ru>
2021-10-06 10:50:55 +0100

    create a header of request side

--- insomnia-web/src/App.tsx
+++ insomnia-web/src/App.tsx
@@ -2,7 +2,7 @@ import { RequestSide, ResponseSide } from './components';
 
 export function App() {
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2">
       <RequestSide />
       <ResponseSide />
     </div>

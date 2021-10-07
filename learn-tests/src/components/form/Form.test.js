3e08124acdaff047b47ecb48c2d7fafed9fae448
Mario-aj <mariojorge1997@mail.ru>
2021-10-07 15:02:50 +0100

    add faker lib to genarete fake data to test our Form

--- learn-tests/src/components/form/Form.test.js
+++ learn-tests/src/components/form/Form.test.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import fake from 'faker';
 
 import { Form } from './';
 
@@ -14,18 +15,18 @@ describe('<Form />', () => {
   });
 
   it('should be able tto type in the input and submit data', () => {
-    let submitData;
-    const handleSubmit = (data) => (submitData = data);
+    const handleSubmit = jest.fn();
 
     render(<Form onSubmit={handleSubmit} />);
 
-    const username = 'mario jorge';
-    const password = 'Admirafelicidade';
+    const username = fake.internet.userName();
+    const password = fake.internet.password();
 
     userEvent.type(screen.getByLabelText(/username/i), username);
     userEvent.type(screen.getByLabelText(/password/i), password);
     userEvent.click(screen.getByRole('button', { name: /login/i }));
 
-    expect(submitData).toEqual({ username, password });
+    expect(handleSubmit).toHaveBeenCalledWith({ username, password });
+    expect(handleSubmit).toHaveBeenCalledTimes(1);
   });
 });

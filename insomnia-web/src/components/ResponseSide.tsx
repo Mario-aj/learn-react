1735fd4643e2b39afdb6383fda06998b25b16ee6
Mario-aj <mariojorge1997@mail.ru>
2021-10-09 10:49:14 +0100

    use rjv to show data to users

--- insomnia-web/src/components/ResponseSide.tsx
+++ insomnia-web/src/components/ResponseSide.tsx
@@ -1,21 +1,31 @@
 import { useEffect, useState } from 'react';
+import ReactJson from 'react-json-view';
 import { useRequest } from '../hook';
 import { fetch } from '../services';
 
 const ResponseSide = (): JSX.Element => {
   const { method, url } = useRequest();
-  const [data, setData] = useState(null);
+  const [requetResponse, setRequestResponse] = useState(null);
   const [status, setStatus] = useState('');
+  const [requestTime, setRequestTime] = useState('');
+  const [requestError, setRequestError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { data: items, request } = await fetch({ method, url });
+        const requestStartedAt = new Date().getTime();
+        const response = await fetch({ method, url });
+        const requestEndedAt = new Date().getTime();
+        const { data, request } = response;
 
         setStatus(`${request.status} ${request.statusText}`);
-        setData(items);
+        setRequestTime(`${(requestEndedAt - requestStartedAt) / 100} s`);
+        setRequestResponse(data);
+        setRequestError(false);
       } catch (err) {
-        console.log(err);
+        setRequestResponse(null);
+        setStatus('404 Not Found');
+        setRequestError(true);
       }
     };
 
@@ -25,18 +35,27 @@ const ResponseSide = (): JSX.Element => {
   return (
     <div className="flex flex-col overflow-hidden border border-gray-700">
       <div className="flex items-center justify-start gap-4 pl-4 bg-gray-900 border border-gray-700 h-11">
-        <span className="px-2 bg-green-600 rounded-sm">{status}</span>
-        <span className="px-2 bg-gray-800 rounded-sm">1.61 s</span>
-        <span className="px-2 bg-gray-800 rounded-sm">105.8 KB</span>
+        {!!status && (
+          <span
+            className={`px-1 ${
+              !requestError ? 'bg-green-600' : 'bg-yellow-600'
+            } rounded-sm`}
+          >
+            {status}
+          </span>
+        )}
+        {!!requestTime && (
+          <span className="px-1 bg-gray-800 rounded-sm">{requestTime}</span>
+        )}
       </div>
-      {data && (
-        <div
-          className="h-screen p-2 overflow-y-scroll break-words"
-          id="response-container"
-        >
-          <pre className="flex-wrap break-normal break-words break-all">
-            {JSON.stringify(data, null, 4)}
-          </pre>
+
+      {requetResponse && (
+        <div className="h-screen overflow-y-scroll" id="response-container">
+          <ReactJson
+            theme="tomorrow"
+            src={requetResponse}
+            displayDataTypes={false}
+          />
         </div>
       )}
     </div>

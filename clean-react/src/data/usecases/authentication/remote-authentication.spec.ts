da8e42e5b0b1f9aea3b797e111cd08a932957078
Mario-aj <mariojorge1997@mail.ru>
2021-10-27 09:33:00 +0100

    test: should call classes with correct URL

---
+++ clean-react/src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,20 @@
+import { HttpPostClient } from 'data/protocols/http/http-post-client';
+import { RemoteAuthentication } from './remote-authentication';
+
+describe('RemoteAuthentication', () => {
+  it('should call HttpPostCLient with correct URL', async () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string;
+
+      async post(url: string): Promise<void> {
+        this.url = url;
+      }
+    }
+
+    const url = 'any_url';
+    const httpPostClientSpy = new HttpPostClientSpy();
+    const sut = new RemoteAuthentication(url, httpPostClientSpy);
+    await sut.auth();
+    expect(httpPostClientSpy.url).toBe(url);
+  });
+});

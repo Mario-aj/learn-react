3f5d75b7f10987c16b7bd985727f7b66329d6c39
Mario-aj <mariojorge1997@mail.ru>
2021-10-27 09:46:01 +0100

    refactor: mocked classes and functions

--- clean-react/src/data/usecases/authentication/remote-authentication.spec.ts
+++ clean-react/src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,20 +1,27 @@
-import { HttpPostClient } from 'data/protocols/http/http-post-client';
 import { RemoteAuthentication } from './remote-authentication';
+import { HttpPostClientSpy } from '../../test/mock-http-client';
+
+type SutTypes = {
+  sut: RemoteAuthentication;
+  httpPostClientSpy: HttpPostClientSpy;
+};
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy();
+  const sut = new RemoteAuthentication(url, httpPostClientSpy);
+
+  return {
+    sut,
+    httpPostClientSpy,
+  };
+};
 
 describe('RemoteAuthentication', () => {
   it('should call HttpPostCLient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string;
-
-      async post(url: string): Promise<void> {
-        this.url = url;
-      }
-    }
-
-    const url = 'any_url';
-    const httpPostClientSpy = new HttpPostClientSpy();
-    const sut = new RemoteAuthentication(url, httpPostClientSpy);
+    const url = 'other_url';
+    const { sut, httpPostClientSpy } = makeSut(url);
     await sut.auth();
+
     expect(httpPostClientSpy.url).toBe(url);
   });
 });

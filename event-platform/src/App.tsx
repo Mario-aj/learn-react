67b1e4b3f7460538fa6d9ba86c6f6cce7edce56a
mario-aj <mariojorge1997@mail.ru>
2022-06-25 10:48:05 +0100

    render event page

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,3 +1,9 @@
+import { Event } from "./pages/Event";
+
 export function App() {
-  return <h1 className="text-4xl font-bold">Hello world!</h1>;
+  return (
+    <h1 className="text-4xl font-bold">
+      <Event />
+    </h1>
+  );
 }

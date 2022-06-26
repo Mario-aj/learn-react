304f0743fa5b3ef6e476201319f4bf0a809b854a
mario-aj <mariojorge1997@mail.ru>
2022-06-26 11:09:54 +0100

    config assets

--- event-platform/tailwind.config.js
+++ event-platform/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: "url('src/assets/blur-background.png')",
+        "react-logo": "url('src/assets/react-logo.svg')",
+      },
       fontFamily: {
         sans: ["Roboto", "sans-serif"],
       },

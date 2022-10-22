b53d045de534305db77491a4cf02aa1d618910d2
mario-aj <mariojorge1997@mail.ru>
2022-10-22 19:17:42 +0100

    add tailwind into the prejet

--- til_2022/src/App.tsx
+++ til_2022/src/App.tsx
@@ -1,7 +1,9 @@
 export function App() {
   return (
     <div className="App">
-      <h1>Dynamic form</h1>
+      <h1 className="text-3xl font-bold underline text-red-400">
+        Hello Dynamic Form!
+      </h1>
     </div>
   );
 }

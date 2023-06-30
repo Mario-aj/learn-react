6b366f1d24a2b508173604e876bde3231340e523
mario-aj <mariojorge1997@mail.ru>
2023-06-30 20:11:59 +0100

    config themes

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
+  const { theme, colorMode } = useMode();
+
   return (
-    <div className="app">
-      <h1>Hello world!</h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <h1>Hello world!</h1>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 

3d51ab3f480f13d092cb54525cb1e16654fdfa8d
mario-aj <mariojorge1997@mail.ru>
2023-07-24 08:18:15 +0100

    create header component and use it in dashboard home page

--- react-admin/src/scenes/dashboard/index.tsx
+++ react-admin/src/scenes/dashboard/index.tsx
@@ -1,5 +1,13 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;

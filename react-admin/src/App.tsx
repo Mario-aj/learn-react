6c94f31bdc4d8a108521049f698ac7c52f829e0c
mario-aj <mariojorge1997@mail.ru>
2023-07-27 08:04:49 +0100

    render form

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -8,8 +8,8 @@ import Sidebar from "./scenes/global/Sidebar";
 import Team from "./scenes/Team";
 import Contacts from "./scenes/Contacts";
 import Invoices from "./scenes/Invoices";
+import Form from "./scenes/Form";
 // import Bar from "./scenes/Bar";
-// import Form from "./scenes/Form";
 // import Line from "./scenes/Line";
 // import Pie from "./scenes/Pie";
 // import FAQ from "./scenes/FAQ";
@@ -33,10 +33,10 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
 
               {/* 
               <Route path="/bar" element={<Bar />} />
-              <Route path="/form" element={<Form />} />
               <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />

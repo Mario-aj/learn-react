f664b55f6bd97f3bfdccfb601d696aa1ab897f5a
mario-aj <mariojorge1997@mail.ru>
2023-08-04 08:08:01 +0100

    render FQ page

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -10,10 +10,10 @@ import Contacts from "./scenes/Contacts";
 import Invoices from "./scenes/Invoices";
 import Form from "./scenes/Form";
 import Calendar from "./scenes/Calendar";
+import FAQ from "./scenes/FAQ";
 // import Bar from "./scenes/Bar";
 // import Line from "./scenes/Line";
 // import Pie from "./scenes/Pie";
-// import FAQ from "./scenes/FAQ";
 // import Geography from "./scenes/Geography";
 
 function App() {
@@ -35,12 +35,12 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
 
               {/* 
               <Route path="/bar" element={<Bar />} />
               <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
-              <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
             */}
             </Routes>

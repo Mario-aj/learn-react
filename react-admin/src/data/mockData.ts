a9578369deba75c3c434dc738724823850fc1966
mario-aj <mariojorge1997@mail.ru>
2023-08-04 08:07:41 +0100

    fake questions for FAQ page

--- react-admin/src/data/mockData.ts
+++ react-admin/src/data/mockData.ts
@@ -1281,3 +1281,26 @@ export const mockGeographyData = [
     value: 171135,
   },
 ];
+
+export const mockQuestions = [
+  {
+    question: "What is Lorem Ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    question: "Where does it come from?",
+    answer:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+  },
+  {
+    question: "Why do we use it?",
+    answer:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+  },
+  {
+    question: "Where can I get some?",
+    answer:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.",
+  },
+];

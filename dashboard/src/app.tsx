43a7aaa8bbc43015be43d83a9b8092bb9c7abe60
mario-aj <mariojorge1997@mail.ru>
2024-06-17 22:08:49 +0100

    just a commit

--- dashboard/src/app.tsx
+++ dashboard/src/app.tsx
@@ -1,7 +1,30 @@
+import React, { useEffect } from "react";
+import { FiSettings } from "react-icons/fi";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
 import "./app.css";
 
 export const App = () => {
-  return <h1 className="underline text-3xl text-red-500">App</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4">
+            <TooltipComponent content="Settings" position="TopCenter">
+              <button
+                type="button"
+                style={{ background: "blue", borderRadius: "50%" }}
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
